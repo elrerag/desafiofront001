19a6f2de4f22e3649fab90e08fe5c8089bc5afcf
Luis Herrera <l.herrera.garnica@gmail.com>
2023-08-31 23:28:19 +0000

    ✨ fin cinco

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'
+import ReactDOM from 'react-dom';
+import './index.css';
 
 function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const [inputValue, setInputValue] = useState("");
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  }
+  const handleSum = () => {
+    const sum = parseFloat(number1) + parseFloat(number2);
+    setTotal(sum);
+  };
 
   return (
-    <>
-      <h3>Mi Desafío numero 4</h3>
+    <div>
+      <h2>Sumar dos numeros</h2>
       <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
+        placeholder="Primer numero"
+        type="number"
+        value={number1}
+        onChange={(e) => setNumber1(e.target.value)}
       />
-      <button disabled={inputValue === ""}>Enviar</button>
-    </>
+      <input
+        placeholder="Segundo numero"
+        type="number"
+        value={number2}
+        onChange={(e) => setNumber2(e.target.value)}
+      />
+      <button onClick={handleSum}>Sumar</button>
+      <p>Total: {total}</p>
+    </div>
   );
 }
 
@@ -27,4 +37,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
