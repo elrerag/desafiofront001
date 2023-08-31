62087348b935dcede31734816e7bed740ef02ba3
Luis Herrera <l.herrera.garnica@gmail.com>
2023-08-31 23:25:26 +0000

    ✨ fin cuatro

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+function App() {
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  return (
+    <>
+      <h3>Mi Desafío numero 4</h3>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button disabled={inputValue === ""}>Enviar</button>
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
