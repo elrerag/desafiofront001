5ef1e0eafbd2fe057ff09d4aebf9a0f023717db3
Luis Herrera <l.herrera.garnica@gmail.com>
2023-08-31 23:15:02 +0000

    ✨ Fin desafío uno

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+function DataList(props) {
+  return (
+    <ul>
+      {props.data.map((person, index) => (
+        <li key={index}>
+          <span>{person.name}</span> <span>{person.age}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+const data = [
+  { name: 'Peter', age: 25 },
+  { name: 'Jenna', age: 24 },
+  { name: 'Sandra', age: 31 },
+];
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <DataList data={data} />
+  </React.StrictMode>
+);
